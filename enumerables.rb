ec60aa864ee1f80c5a76a3ec33b60ffcc3d0d69e
codedit334 <oussama7elabdioui@gmail.com>
2023-05-11 13:44:23 +0100

    Fix linter errors

--- enumerables.rb
+++ enumerables.rb
@@ -1,17 +1,17 @@
 module MyEnumerable
-  def all?(&block)
-    each {|item| return false unless yield(item)}
+  def all?()
+    each { |item| return false unless yield(item) }
     true
   end
 
-  def any?(&block)
-    each{|item| return true if yield(item)}
+  def any?()
+    each { |item| return true if yield(item) }
     false
   end
 
-  def filter(&block)
+  def filter()
     result = []
-    each{|item| result << item if yield(item)}
+    each { |item| result << item if yield(item) }
     result
   end
 end
--- mylist.rb
+++ mylist.rb
@@ -1,5 +1,5 @@
 $LOAD_PATH << '.'
-require 'enumerables.rb'
+require 'enumerables'
 
 class MyList
   include MyEnumerable
@@ -15,10 +15,10 @@ end
 
 list = MyList.new(1, 2, 3, 4)
 
-puts list.all? {|e| e < 5}
-puts list.all? {|e| e > 5}
+puts(list.all? { |e| e < 5 })
+puts(list.all? { |e| e > 5 })
 
-puts list.any? {|e| e == 2}
-puts list.any? {|e| e == 5}
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
 
-puts list.filter {|e| e.even?}
+puts(list.filter(&:even?))
